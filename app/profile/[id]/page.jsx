4fb259a96b456472f084cbec433af6e690651666
danielguan23 <danielguan23@gmail.com>
2024-03-18 17:19:10 +0800

    v1 deployment bug

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'next/navigation'
-import Profile from '@components/Profile'
+import Profile from '@components/Profile';
 
 const OthersProfiles = () => {
   const {id} = useParams();
